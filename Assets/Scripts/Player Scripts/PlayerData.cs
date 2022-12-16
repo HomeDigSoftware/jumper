09d8b4e610eb16d85d4a6ae34d6be4a613d558ec
tzaf <tzaf24@gmail.com>
2022-12-16 07:49:43 +0200

    trying to connect server with ngrok : testing the setup

---
+++ Assets/Scripts/Player Scripts/PlayerData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct PlayerData
+{
+    public string id;
+    public float xPos;
+    public float yPos;
+    public float zPos;
+    public double timestamp;
+}
